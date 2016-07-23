60b80558b9b0d6c31d109dfd463ea04fa381cfc4
Takuya Kitazawa <k.takuti@gmail.com>
2016-07-23 15:06:58 +0900

    :rocket: v0.2.0

--- setup.py
+++ setup.py
@@ -2,12 +2,9 @@ from setuptools import setup
 
 setup(
     name='ansibrew',
-    version='0.1',
+    version='0.2.0',
     py_modules=['ansibrew'],
-    install_requires=[
-        'Click',
-        'pyyaml'
-    ],
+    install_requires=['Click'],
     entry_points='''
         [console_scripts]
         ansibrew=ansibrew:cli
