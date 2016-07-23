ecf8687c2f00cbee2466c7467d3c5d51babde859
Takuya Kitazawa <k.takuti@gmail.com>
2016-07-23 14:59:01 +0900

    #1: support install_options with json-based `brew info` parsing

--- ansibrew.py
+++ ansibrew.py
@@ -2,6 +2,7 @@
 
 import click
 import subprocess
+import json
 
 
 @click.group(invoke_without_command=True)
@@ -30,23 +31,43 @@ def taps():
 def packages():
     cmd = 'brew list'
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    packages = p.communicate()[0].rstrip().split('\n')
+    installed_package_names = p.communicate()[0].rstrip().split('\n')
 
-    excepted_packages = []
-    for package in packages:
-        cmd = 'brew info %s' % package
+    installed_package_full_names = []
+    depended_package_full_names = []
+    options, names = {}, {}
+    for name in installed_package_names:
+        cmd = 'brew info --json=v1 %s' % name
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        info = p.communicate()[0].rstrip().split('\n')
+        info = p.communicate()[0].rstrip()
 
-        if '==> Dependencies' not in info:
+        # there is no information for a package
+        if len(info) == 0:
             continue
 
-        parsed_required = info[info.index('==> Dependencies') + 1].split(' ')
-        excepted_packages += [parsed_required[i] for i in range(1, len(parsed_required), 2)]
+        j = json.loads(info)[0]
 
+        # get detailed information of the most stable installed version
+        stable_version = j['versions']['stable']
+        stable = [installed for installed in j['installed'] if installed['version'] == stable_version]
+
+        # if there is no versions which are exactly matched to the stable version, pick one (HEAD) from installed versions
+        used_options = j['installed'][0]['used_options'] if len(stable) == 0 else stable[0]['used_options']
+
+        # remove heading '--'
+        options[j['full_name']] = [opt[2:] for opt in used_options]
+
+        depended_package_full_names += j['dependencies']
+        installed_package_full_names.append(j['full_name'])
+        names[j['full_name']] = j['name']
+
+    # depended packages will be installed automatically
     print('homebrew_packages:')
-    for p in packages:
-        print('  - { name: %s }' % p)
+    for full_name in (set(installed_package_full_names) - set(depended_package_full_names)):
+        if len(options[full_name]) == 0:
+            print('  - { name: %s }' % names[full_name])
+        else:
+            print('  - { name: %s, install_options: [%s] }' % (names[full_name], ', '.join(options[full_name])))
 
 
 @cli.command()
